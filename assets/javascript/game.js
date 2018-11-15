0616c7a30a06b1877831fdbf32207a405fe320df
Chris Forward <cforward5674@gmail.com>
2018-11-14 20:00:50 -0700

    more changes1

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+/*Create variable and function for game random number between 19-120, put in div*/
+
+/*create variables for gems images, create function that generates
+a random between 1-12 for each image when game starts*/
+
+/*Create a "score-box" add to div*/
+
+/*Create onclick event that adds a number to the total score box, when
+each crystal button is clicked, add to div*/
+
+/*Create a function that compares total score to random number*/
+
+/*Create a wins/losses function that keeps track of them.  If gem
+score matches random number add a "win", else add a loss, add to div*/
+
+/*After a win or a loss, reset game, but not wins/losses box.*/
+
