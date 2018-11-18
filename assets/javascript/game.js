0ceb439a362c2da0808a44f056e8a33d3615a381
Chris Forward <cforward5674@gmail.com>
2018-11-17 17:01:44 -0700

    css, js changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,38 @@
-/*Create variable and function for game random number between 19-120, put in div*/
+/*Create variable for the number to reach, put in div*/
 
+// console.log(crystalNumbers);
+var randomNumber = Math.floor(Math.random()*100);
+$("#current-number").html("<h3>" + randomNumber + "</h3>");
+
+/*Create variable and function for game random number between 19-120, put in div*/
+var counter = 0;
+console.log(counter);
+var crystalNumbers = [10, 15, 5, 2];
+// var crystalNumbers = function (min, max) {
+//     return Math.floor(Math.random()*(1, 12))+ min;
+// }
+
+counter++;
+for (i = 0; i < crystalNumbers.length; i++) {
+    var crystalImage = $("<img>");
+   
+    crystalImage.addClass("crystal-image");
+
+    crystalImage.attr("src","assets/images/drusy.jpg");
+
+    crystalImage.attr("crystalvalue", crystalNumbers[i]);
+
+    $("#gems").append(crystalImage);
+
+    $(".crystal-image").on("click", function() {
+
+    var imageValue = ($(this).attr("imagevalue"));
+    ImageValue = parseInt(imageValue);
+
+    counter += imageValue;
+    $("#score").html("<h3>" + imageValue + "</h3>");
+    });
+}
 /*create variables for gems images, create function that generates
 a random between 1-12 for each image when game starts*/
 
@@ -13,5 +46,9 @@ each crystal button is clicked, add to div*/
 /*Create a wins/losses function that keeps track of them.  If gem
 score matches random number add a "win", else add a loss, add to div*/
 
-/*After a win or a loss, reset game, but not wins/losses box.*/
+/*After a win or a loss, reset game, but not wins/losses box.
 
+create object for each crystal and assign it a value
+set random numbers for each crystal
+
+crystal.green.value= getRandom(1,12) */
